7ee3b4306859ec799c574dabcc1d85cb0b8307de
DashSatya <darshans1989@gmail.com>
2017-05-09 20:25:52 +0530

    gradle repo install

--- src/main/java/com/angular/services/ServicesMain.java
+++ src/main/java/com/angular/services/ServicesMain.java
@@ -1,7 +1,16 @@
 package com.angular.services;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Darsh on 5/8/2017.
  */
+
+@SpringBootApplication
 public class ServicesMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesMain.class, args);
+    }
 }
