603ef484a06f854027ff83e64c437a62e00e8b4e
Darshan Satya <darshans1989@gmail.com>
2017-05-14 21:28:58 +0530

    post changes

--- src/main/java/com/angular/services/controller/MainController.java
+++ src/main/java/com/angular/services/controller/MainController.java
@@ -3,8 +3,7 @@ package com.angular.services.controller;
 import com.angular.services.dao.UserDao;
 import com.angular.services.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Darsh on 5/13/2017.
@@ -15,9 +14,11 @@ public class MainController {
     @Autowired
     UserDao userDao;
 
-    @RequestMapping("/save")
-    public String add(){
-        userDao.save(new UserEntity("dash","asd@gmail.com","asd"));
+    @RequestMapping(value="/api/save",method = RequestMethod.POST,consumes = "application/json")
+    @ResponseBody
+    public String addUser(@RequestBody UserEntity userEntity){
+        System.out.println("adding"+userEntity);
+        userDao.save(userEntity);
         return "Done";
     }
 
